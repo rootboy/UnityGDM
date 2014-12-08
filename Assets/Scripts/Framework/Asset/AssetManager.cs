a20dacf2df7d90ac409f9517db45d7f5650e7e89
Rootboy <m13603893145@163.com>
2014-12-09 00:28:05 +0800

    update

---
+++ Assets/Scripts/Framework/Asset/AssetManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using GDM.Global;
+
+namespace GDM
+{
+	namespace Asset
+	{
+		public class AssetManager :  Singleton<AssetManager>
+		{
+			protected Dictionary<int, Object> dicAssets = new Dictionary<int, Object>();
+	
+
+
+			public Object Load(string path)
+			{
+#if UNITY_EDITOR
+
+
+
+#else
+
+
+
+#endif
+				return null;
+			}
+
+
+			public T Load<T>(string path)
+			{
+				return default(T);
+			}
+
+
+
+
+
+
+			private Object LoadAssetAtPath(string path, System.Type type)
+			{
+				return Resources.LoadAssetAtPath(path, type);
+			}
+
+
+			private T LoadAssetAtPath<T>(string path) where T : Object
+			{
+				return Resources.LoadAssetAtPath<T>(path);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Framework/Util/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace SmartWar
+{
+	namespace Util
+	{
+		public class StringHelper 
+		{
+			public static string Secs2String(uint secs)
+			{
+				return null;
+			}
+		}
+	}
+}
