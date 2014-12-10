1605c87dd9a4e9999e3f32790a3d4a19a5aa5aa7
Rootboy <m13603893145@163.com>
2014-12-10 23:22:05 +0800

    aa

--- Assets/Scripts/Framework/Tools/Log.cs
+++ Assets/Scripts/Framework/Tools/Log.cs
@@ -17,13 +17,26 @@ namespace GDM
 			LogType logType = LogType.Log;
 
 			StreamWriter sw;
-			bool init;
+			string path = "";
 
 
 			void Awake()
 			{
-				sw = new StreamWriter(Application.streamingAssetsPath + "/log.txt", true);
+				path = Application.streamingAssetsPath + "/log.txt";
 				Application.RegisterLogCallback(HandleLog);
+
+				if(!File.Exists(path)){
+					sw = new StreamWriter(Application.streamingAssetsPath + "/log.txt", true);
+					sw.WriteLine(string.Format("deviceModel: {0}", SystemInfo.deviceModel));
+					sw.WriteLine(string.Format("deviceName: {0}", SystemInfo.deviceName));
+					sw.WriteLine(string.Format("graphicsDeviceVendor: {0}", SystemInfo.graphicsDeviceVendor));
+					sw.WriteLine(string.Format("graphicsDeviceName: {0}", SystemInfo.graphicsDeviceName));
+					sw.WriteLine(string.Format("systemMemorySize: {0}", SystemInfo.systemMemorySize));
+					sw.WriteLine(string.Format("graphicsMemorySize: {0}", SystemInfo.graphicsMemorySize));
+					sw.WriteLine();
+					sw.WriteLine();
+				}
+				if(sw == null) sw = new StreamWriter(path, true);
 			}
 
 			void OnDestroy()
@@ -39,19 +52,17 @@ namespace GDM
 
 				if(sw != null){
 					sw.WriteLine(System.DateTime.UtcNow.ToString());
+					sw.WriteLine(string.Format("logType: {0}", type.ToString()));
 					sw.WriteLine(string.Format("output: {0}", output));
 					sw.WriteLine(string.Format("stack: {0}", stackTrace));
-					sw.WriteLine(string.Format("logType: {0}", type.ToString()));
-
-					sw.WriteLine(string.Format("deviceModel: {0}", SystemInfo.deviceModel));
-					sw.WriteLine(string.Format("deviceName: {0}", SystemInfo.deviceName));
-					sw.WriteLine(string.Format("graphicsDeviceVendor: {0}", SystemInfo.graphicsDeviceVendor));
-					sw.WriteLine(string.Format("graphicsDeviceName: {0}", SystemInfo.graphicsDeviceName));
-					sw.WriteLine(string.Format("systemMemorySize: {0}", SystemInfo.systemMemorySize));
-					sw.WriteLine(string.Format("graphicsMemorySize: {0}", SystemInfo.graphicsMemorySize));
-					sw.WriteLine();
 				}
-				sw.Close();
+			}
+
+			void OnApplicationQuit()
+			{
+				if(sw != null){
+					sw.Close();
+				}
 			}
 		}
 	}
