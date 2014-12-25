d036356c6a9854c1707944a2a577fff327e783fe
Rootboy <m13603893145@163.com>
2014-12-25 21:18:12 +0800

    update

--- Assets/Scripts/Generic/Util/StringUtil.cs
+++ Assets/Scripts/Generic/Util/StringUtil.cs
@@ -8,10 +8,35 @@ namespace SmartWar
 	{
 		public class StringUtil 
 		{
-			public static string Secs2String(uint secs)
+            /// <summary>
+            /// 将秒转成"分：秒"格式的字符串,如参数为88，返回值为01:28.
+            /// </summary>
+            /// <param name="secs"></param>
+            /// <returns></returns>
+			public static string SecsToStringMS(uint secs)
 			{
 				return null;
 			}
+
+            /// <summary>
+            /// 将秒转成"时：分：秒"格式的字符串,如参数为88，返回值为00:01:28.
+            /// </summary>
+            /// <param name="secs"></param>
+            /// <returns></returns>
+            public static string SecsToStringHMS(uint secs)
+            {
+                return null;
+            }
+
+            /// <summary>
+            /// 将"(1.0, 2.0, 3.0)"格式的字符串转换为Vector3格式.
+            /// </summary>
+            /// <param name="str"></param>
+            /// <returns></returns>
+            public static Vector3 StringToVector3(string str)
+            {
+                return Vector3.one;
+            }
 
 			//Add code here...
 		}
