86ae223d877fb4f22a1c834c8640fc02e06b1b2e
Rootboy <m13603893145@163.com>
2015-01-04 17:49:58 +0800

    update

---
+++ Assets/Scripts/Generic/Util/TransformUtil.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GDM
+{
+    namespace Util
+    {
+        public static class TransformUtil
+        {
+            /// <summary>
+            /// Add a child to parent, keep the value of transform equally.
+            /// </summary>
+            /// <param name="child"></param>
+            /// <param name="parent"></param>
+            public static void AddChild(Transform child, Transform parent)
+            {
+                Vector3 pos = child.localPosition;
+                Vector3 rot = child.localEulerAngles;
+                Vector3 sca = child.localScale;
+
+                child.parent = parent;
+                child.localPosition = pos;
+                child.localEulerAngles = rot;
+                child.localScale = sca;
+            }
+
+
+            /// <summary>
+            /// Add a child to parent, keep the value of transform equally.
+            /// </summary>
+            /// <param name="child"></param>
+            /// <param name="parent"></param>
+            public static void AddChild(GameObject child, GameObject parent)
+            {
+                AddChild(child.transform, parent.transform);
+            }
+        }
+    }
+}
